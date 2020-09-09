5e374c4a68acb54b816a80bb3c93677f28a38f7a
Odise <odise9411272@gmail.com>
2020-09-09 23:49:18 +0800

    create

---
+++ app/src/main/java/com/example/testmainactivity/RecycleVIewHolder.java
@@ -0,0 +1,12 @@
+package com.example.testmainactivity;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecycleVIewHolder extends RecyclerView.ViewHolder {
+    public RecycleVIewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
